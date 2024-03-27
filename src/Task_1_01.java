363b6694b8cab2a181029a94104cd7ad3d8a4169
AIDAR <blacksanta@mail.ru>
2024-03-27 14:26:12 +0300

    Rename file CountDuplicateCharacters.java => Task_1_01.java

--- src/Task_1_01.java
+++ src/Task_1_01.java
@@ -1,11 +1,2 @@
-import java.util.HashMap;
-import java.util.Map;
+public class Task_1_01 {}
 
-public class  CountDuplicateCharacters{
-    map<Character,Integer> CountDuplicateCharacters(String str){
-};
-map<Character,Integer> CountDuplicateCharacters(String str){
-
-
-    Map<Character,Integer> result = new HashMap<>();
-}
